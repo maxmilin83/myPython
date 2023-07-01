f2900f7fef21bd28b861697becd31c14747a9a9d
Max <maxed492@gmail.com>
2023-07-01 15:07:43 +0100

    Finished

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 from data import questions
-from quiz import Question
+from quiz import Question,QuizBrain
 
 question_bank = [] 
 
 for question in questions:
     questionObject = Question(question['text'],question['answer'])
     question_bank.append(questionObject)
+
+quiz = QuizBrain(question_bank)
+playAgain = "yes"
+
+while playAgain == "yes":
+    while quiz.is_more_questions():
+        quiz.ask_question()
+        print("\n")
+
+    print(f"Final Score: {quiz.correct_questions}/{quiz.question_number}")
+    
+    playAgain = input("Play again? (yes/no): ")
+    playAgain = playAgain.lower()
+    if playAgain == "yes":
+        quiz = QuizBrain(question_bank)
+        continue
+    else:
+        break
+
 
