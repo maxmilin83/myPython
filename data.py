f1029af9379090c15dd1212fe804bc3c47912c93
Max <maxed492@gmail.com>
2023-07-01 14:29:38 +0100

    Filled question bank in data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+API = "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=boolean"
+request = requests.get(f"{API}")
+request = request.json()
+request = request['results']
+
+questions = []
+
+for question in request:
+    questions.append({'text':question['question'],'answer':question['correct_answer']})
+
+
